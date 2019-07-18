b3e8a81dfc0a3ddd9ebae23832b59274fe01b695
Marcel <marcel@radzio-sh.de>
2019-07-18 18:49:04 +0200

    [Store] Increase DB version

--- lib/src/Store.dart
+++ lib/src/Store.dart
@@ -55,7 +55,7 @@ class Store {
   _init() async {
     var databasePath = await getDatabasesPath();
     String path = p.join(databasePath, "FluffyMatrix.db");
-    _db = await openDatabase(path, version: 5,
+    _db = await openDatabase(path, version: 6,
         onCreate: (Database db, int version) async {
       await createTables(db);
     }, onUpgrade: (Database db, int oldVersion, int newVersion) async {
