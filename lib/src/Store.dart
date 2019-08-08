f2bbe978a87fb162924f5c4870fd8bb0b1b06397
Christian Pauly <Christian-pauly@posteo.de>
2019-08-08 13:31:04 +0200

    [Store] Fix query bug

--- lib/src/Store.dart
+++ lib/src/Store.dart
@@ -340,11 +340,7 @@ class Store {
   /// Returns a list of events for the given room and sets all participants.
   Future<List<Event>> getEventList(Room room) async {
     List<Map<String, dynamic>> eventRes = await db.rawQuery(
-        "SELECT * " +
-            " FROM Events " +
-            " WHERE room_id=?" +
-            " GROUP BY id " +
-            " ORDER BY origin_server_ts DESC",
+        "SELECT * " + " FROM Events " + " WHERE room_id=?" + " GROUP BY id",
         [room.id]);
 
     List<Event> eventList = [];
