8b67d58ecc6c5348341ee4ffbe8d07acc88d010a
Christian Pauly <christian-pauly@posteo.de>
2019-10-23 18:14:04 +0000

    [MatrixFile] Path always lower case

--- lib/src/utils/MatrixFile.dart
+++ lib/src/utils/MatrixFile.dart
@@ -2,6 +2,6 @@ class MatrixFile {
   List<int> bytes;
   String path;
 
-  MatrixFile({this.bytes, this.path});
+  MatrixFile({this.bytes, String path}) : this.path = path.toLowerCase();
   int get size => bytes.length;
 }
