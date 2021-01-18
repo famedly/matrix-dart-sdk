484037a13f9b1c9c4fe85de03bde3869365a259d
Sorunome <mail@sorunome.de>
2021-01-18 16:33:12 +0100

    feat: Add m.dummy event

--- lib/src/model/event_types.dart
+++ lib/src/model/event_types.dart
@@ -1,17 +1,17 @@
 /* MIT License
-* 
+*
 * Copyright (C) 2019, 2020, 2021 Famedly GmbH
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in all
 * copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -59,6 +59,9 @@ abstract class EventTypes {
   static const String KeyVerificationAccept = 'm.key.verification.accept';
   static const String SecretRequest = 'm.secret.request';
   static const String SecretSend = 'm.secret.send';
+  static const String Dummy = 'm.dummy';
+
+  // Account data event types
   static const String CrossSigningSelfSigning = 'm.cross_signing.self_signing';
   static const String CrossSigningUserSigning = 'm.cross_signing.user_signing';
   static const String CrossSigningMasterKey = 'm.cross_signing.master';
