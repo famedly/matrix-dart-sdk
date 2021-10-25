7a85d52d93e974ff67bdb73440b5c6cc87d4a476
Krille Fear <christian-pauly@posteo.de>
2021-10-25 11:11:55 +0200

    fix: Remove unnecessary null check

--- lib/encryption/key_manager.dart
+++ lib/encryption/key_manager.dart
@@ -741,7 +741,7 @@ class KeyManager {
               client.getUserDeviceKeysByCurve25519Key(dbSession.senderKey);
           args.dbSessions.add(_DbInboundGroupSessionBundle(
             dbSession: dbSession,
-            verified: device?.verified ?? false,
+            verified: device.verified ?? false,
           ));
           i++;
           if (i > 10) {
