000531ce71c870369178b2c9baa8f31742a6188b
Krille Fear <christian-pauly@posteo.de>
2021-11-18 08:18:36 +0100

    CI: Play with CI

--- scripts/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-# pub run test -p vm
-flutter test --coverage
-flutter pub global activate remove_from_coverage
-flutter pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '\.g\.dart$'
-genhtml -o coverage coverage/lcov.info || true
