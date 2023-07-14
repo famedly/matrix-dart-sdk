4bbb1fbba78a80a8fa8b95b1e41e9c69baec59f0
Nicolas Werner <n.werner@famedly.com>
2023-07-14 13:40:39 +0200

    feat: Use github actions

--- scripts/integration-server-synapse.sh
+++ scripts/integration-server-synapse.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-docker run -d --name synapse --tmpfs /data \
+
+docker run -d --name synapse --tmpfs /data $NETWORK \
     --volume="$(pwd)/test_driver/synapse/data/homeserver.yaml":/data/homeserver.yaml:rw \
     --volume="$(pwd)/test_driver/synapse/data/localhost.log.config":/data/localhost.log.config:rw \
     -p 80:80 matrixdotorg/synapse:latest
--- scripts/test_driver.sh
+++ scripts/test_driver.sh
@@ -2,5 +2,5 @@
 if which flutter >/dev/null; then
     flutter --no-version-check test test_driver/matrixsdk_test.dart --dart-define=HOMESERVER=$HOMESERVER --dart-define=USER1_NAME=$USER1_NAME --dart-define=USER2_NAME=$USER2_NAME --dart-define=USER3_NAME=$USER3_NAME --dart-define=USER1_PW=$USER1_PW --dart-define=USER2_PW=$USER2_PW --dart-define=USER3_PW=$USER3_PW
 else
-    dart run -D HOMESERVER=$HOMESERVER -D USER1_NAME=$USER1_NAME -D USER2_NAME=$USER2_NAME -D USER3_NAME=$USER3_NAME -D USER1_PW=$USER1_PW -D USER2_PW=$USER2_PW -D USER3_PW=$USER3_PW test_driver/matrixsdk_test.dart -p vm
+    dart -D HOMESERVER=$HOMESERVER -D USER1_NAME=$USER1_NAME -D USER2_NAME=$USER2_NAME -D USER3_NAME=$USER3_NAME -D USER1_PW=$USER1_PW -D USER2_PW=$USER2_PW -D USER3_PW=$USER3_PW test test_driver/matrixsdk_test.dart -p vm
 fi
