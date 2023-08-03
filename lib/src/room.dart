af2a3083dfc49effbaaf7cd394c5b2fd3934f319
MinhDV <drminh2807@gmail.com>
2023-08-03 15:21:03 +0700

    TW-275 [M1] What happen if user typing+ sending very fast a lot of message

--- lib/src/room.dart
+++ lib/src/room.dart
@@ -27,7 +27,6 @@ import 'package:matrix/matrix.dart';
 import 'package:matrix/src/models/timeline_chunk.dart';
 import 'package:matrix/src/utils/cached_stream_controller.dart';
 import 'package:matrix/src/utils/crypto/crypto.dart';
-import 'package:matrix/src/utils/file_send_request_credentials.dart';
 import 'package:matrix/src/utils/markdown.dart';
 import 'package:matrix/src/utils/marked_unread.dart';
 import 'package:matrix/src/utils/space_child.dart';
@@ -878,7 +877,7 @@ class Room {
           txid: messageID,
         );
       } catch (e, s) {
-        if (e is MatrixException || DateTime.now().isAfter(timeoutDate)) {
+        if ((e is MatrixException && e.error != MatrixError.M_LIMIT_EXCEEDED) || DateTime.now().isAfter(timeoutDate)) {
           Logs().w('Problem while sending message', e, s);
           syncUpdate.rooms!.join!.values.first.timeline!.events!.first
               .unsigned![messageSendingStatusKey] = EventStatus.error.intValue;
@@ -887,8 +886,12 @@ class Room {
           _sendingQueue.remove(completer);
           return null;
         }
-        Logs().w('Problem while sending message: $e Try again in 1 seconds...');
-        await Future.delayed(Duration(seconds: 1));
+        final delay = e is MatrixException && e.raw['retry_after_ms'] is int
+            ? e.raw['retry_after_ms'] as int
+            : 1000;
+        Logs().w(
+            'Problem while sending message: $e Try again in $delay milliseconds...');
+        await Future.delayed(Duration(milliseconds: delay));
       }
     }
     syncUpdate.rooms!.join!.values.first.timeline!.events!.first
