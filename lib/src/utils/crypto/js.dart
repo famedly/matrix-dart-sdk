a84d19ef31b62daacb422809aa8bdcc682d486d6
sherlock <quangkhai28999@gmail.com>
2023-08-04 11:03:35 +0700

    hot-fix/missing encrypt/decrypt function in web

--- lib/src/utils/crypto/js.dart
+++ lib/src/utils/crypto/js.dart
@@ -1,10 +1,16 @@
 // Copyright (c) 2020 Famedly GmbH
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+import 'dart:convert';
+import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:matrix/encryption/utils/base64_unpadded.dart';
+import 'package:matrix/matrix.dart';
+import 'package:matrix/src/utils/crypto/base64.dart';
 import 'package:matrix/src/utils/crypto/subtle.dart' as subtle;
 import 'package:matrix/src/utils/crypto/subtle.dart';
+import 'package:matrix/src/utils/models/encrypted_file_info.dart';
 
 abstract class Hash {
   Hash._(this.name);
@@ -31,6 +37,12 @@ class _Sha512 extends Hash {
 }
 
 abstract class Cipher {
+
+  static const String keyType = 'oct';
+  static const String algorithmName = 'A256CTR';
+  static const String messageDigestAlgorithm = 'sha256';
+  static const String version = 'v2';
+
   Cipher._(this.name);
   String name;
   Object params(Uint8List iv);
@@ -38,6 +50,63 @@ abstract class Cipher {
       Uint8List input, Uint8List key, Uint8List iv) async {
     final subtleKey = await importKey('raw', key, name, false, ['encrypt']);
     return (await subtle.encrypt(params(iv), subtleKey, input)).asUint8List();
+  }
+
+  Future<EncryptedFileInfo> encryptStream({
+    required Stream<List<int>> inputStream,
+    required File outputFile,
+    required Uint8List key, 
+    required Uint8List initialVector,
+  }) async {
+    final listBytes = <int>[];
+    inputStream.listen((bytes) {
+      listBytes.addAll(bytes);
+    });
+    final inputBytes = Uint8List.fromList(listBytes);
+    final subtleKey = await importKey('raw', key, name, false, ['encrypt']);
+    final encryptedBytes = await subtle.encrypt(params(initialVector), subtleKey, inputBytes);
+
+    final outputIO = outputFile.openWrite();
+    outputIO.writeAll(encryptedBytes.asUint8List());
+    await outputIO.close();
+
+    return EncryptedFileInfo(
+      key: EncryptedFileKey(
+        algorithrm: algorithmName, 
+        extractable: true, 
+        key: base64Url.encode(key).toBase64Url(), 
+        keyOperations: [KeyOperation.encrypt, KeyOperation.decrypt], 
+        keyType: keyType,
+      ), 
+      version: version, 
+      initialVector: base64.encode(initialVector), 
+      hashes: {
+        messageDigestAlgorithm: base64.encode(await sha256(inputBytes)).toUnpaddedBase64(),
+      }
+    );
+  }
+
+  Future<bool> decryptStream({
+    required Stream<List<int>> inputStream,
+    required File outputFile,
+    required EncryptedFileInfo encryptedFileInfo,
+  }) async {
+    final keyDecoded = base64decodeUnpadded(base64.normalize(encryptedFileInfo.key.key));
+    final initialVectorDecoded = base64decodeUnpadded(base64.normalize(encryptedFileInfo.initialVector));
+
+    final listBytes = <int>[];
+    inputStream.listen((bytes) {
+      listBytes.addAll(bytes);
+    });
+    final inputBytes = Uint8List.fromList(listBytes);
+    final subtleKey = await importKey('raw', keyDecoded, name, false, ['encrypt']);
+    final encryptedBytes = await subtle.encrypt(params(initialVectorDecoded), subtleKey, inputBytes);
+
+    final outputIO = outputFile.openWrite();
+    outputIO.writeAll(encryptedBytes.asUint8List());
+    await outputIO.close();
+
+    return base64.encode(await sha256(inputBytes)).toUnpaddedBase64() == encryptedFileInfo.hashes[messageDigestAlgorithm];
   }
 }
 
