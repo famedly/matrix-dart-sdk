5ce2995baba67827a1eed6e0e1abff754014b6d9
sherlock <quangkhai28999@gmail.com>
2023-10-17 13:43:51 +0700

    feat/add factory method for FileInfo

--- lib/src/utils/models/file_info.dart
+++ lib/src/utils/models/file_info.dart
@@ -1,5 +1,8 @@
 
+import 'dart:typed_data';
+
 import 'package:equatable/equatable.dart';
+import 'package:matrix/matrix.dart';
 import 'package:mime/mime.dart';
 
 class FileInfo with EquatableMixin {
@@ -23,6 +26,30 @@ class FileInfo with EquatableMixin {
         'mimetype': mimeType,
         'size': fileSize,
       });
+  
+
+  FileInfo fromMatrixFile(MatrixFile file) {
+    if (file.msgType == MessageTypes.Image) {
+      return ImageFileInfo(
+        fileName,
+        filePath,
+        fileSize,
+        width: file.info['w'],
+        height: file.info['h'],
+      );
+    } else if (file.msgType == MessageTypes.Video) {
+      return VideoFileInfo(
+        fileName,
+        filePath,
+        fileSize,
+        imagePlaceholderBytes: file.bytes ?? Uint8List(0),
+        width: file.info['w'],
+        height: file.info['h'],
+        duration: file.info['duration'],
+      );
+    }
+    return FileInfo(fileName, filePath, fileSize, readStream: readStream);
+  }
 
   @override
   List<Object?> get props => [fileName, filePath, fileSize, readStream];
