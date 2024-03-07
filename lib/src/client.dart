79b8e69b0b8b5a14e63e4b86f1d832d77aeb950d
Krille <c.kussowski@famedly.com>
2024-03-07 12:06:44 +0100

    fix: add missing supported versions

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -1235,7 +1235,18 @@ class Client extends MatrixApi {
     return pushrules != null ? TryGetPushRule.tryFromJson(pushrules) : null;
   }
 
-  static const Set<String> supportedVersions = {'v1.1', 'v1.2'};
+  static const Set<String> supportedVersions = {
+    'v1.1',
+    'v1.2',
+    'v1.3',
+    'v1.4',
+    'v1.5',
+    'v1.6',
+    'v1.7',
+    'v1.8',
+    'v1.9',
+  };
+
   static const List<String> supportedDirectEncryptionAlgorithms = [
     AlgorithmTypes.olmV1Curve25519AesSha2
   ];
