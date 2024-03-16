e2aa85170a44a10256f59345f6ac04748cccb72d
hieubt <trunghieu.bui201@gmail.com>
2024-03-17 00:08:16 +0700

    Change bullet points when transfer `html` to `text`

--- lib/src/utils/html_to_text.dart
+++ lib/src/utils/html_to_text.dart
@@ -129,7 +129,7 @@ class HtmlToText {
         .join('\n');
   }
 
-  static const _listBulletPoints = <String>['●', '○', '■', '‣'];
+  static const _listBulletPoints = <String>['-', '-', '-', '‣'];
 
   static List<String> _listChildNodes(_ConvertOpts opts, Element node,
       [Iterable<String>? types]) {
