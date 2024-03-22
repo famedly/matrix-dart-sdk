b7befd781e95694108a6317a58c60ffb7a7a5af4
td <j.nirve@famedly.com>
2024-03-22 21:24:21 +0530

    chore: git mv

--- example/matrix_api_lite_example.dart
+++
@@ -1,8 +0,0 @@
-// @dart=2.9
-import 'package:matrix_api_lite/src/matrix_api.dart';
-
-void main() async {
-  final api = MatrixApi(homeserver: Uri.parse('https://matrix.org'));
-  final capabilities = await api.requestServerCapabilities();
-  print(capabilities.toJson());
-}
