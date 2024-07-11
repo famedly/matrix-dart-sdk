a7839181bdf030a4e53361dfd39eb88fb63a81d5
The one with the braid <info@braid.business>
2024-07-11 17:08:47 +0200

    feat: cache .well-known data
    - BREAKING: remove `DiscoveryInformation` from the `checkHomeserver`
      response tuple
    - BREAKING: create `DatabaseApi.storeWellKnown` method
    - BREAKING: create `DatabaseApi.getWellKnown` method
    - add new getter `Client.wellKnown` containing cached
      `DiscoveryInformation`
    - override `Client.homeserver` to invalidate `Client.wellKnown` in case
      the domain changed
    - override `Client.getWellknown` to cache the resolved
      `DiscoveryInformation`
    - add tests for well-known cache
    
    Fixes: https://github.com/famedly/matrix-dart-sdk/issues/1865
    
    Signed-off-by: The one with the braid <info@braid.business>

--- lib/src/database/matrix_sdk_database.dart
+++ lib/src/database/matrix_sdk_database.dart
@@ -1614,6 +1614,25 @@ class MatrixSdkDatabase extends DatabaseApi with DatabaseFileStorage {
   }
 
   @override
+  Future<void> storeWellKnown(DiscoveryInformation? discoveryInformation) {
+    if (discoveryInformation == null) {
+      return _clientBox.delete('discovery_information');
+    }
+    return _clientBox.put(
+      'discovery_information',
+      jsonEncode(discoveryInformation.toJson()),
+    );
+  }
+
+  @override
+  Future<DiscoveryInformation?> getWellKnown() async {
+    final rawDiscoveryInformation =
+        await _clientBox.get('discovery_information');
+    if (rawDiscoveryInformation == null) return null;
+    return DiscoveryInformation.fromJson(jsonDecode(rawDiscoveryInformation));
+  }
+
+  @override
   Future<void> delete() => BoxCollection.delete(
         name,
         sqfliteFactory ?? idbFactory,
