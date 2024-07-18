c1f9770f4be73fd0db9f18c7775ec38a2a22265a
krille-chan <christian-kussowski@posteo.de>
2024-07-18 16:31:05 +0200

    refactor: Remove keys cache from boxes

--- lib/src/database/indexeddb_box.dart
+++ lib/src/database/indexeddb_box.dart
@@ -92,25 +92,14 @@ class Box<V> {
   final BoxCollection boxCollection;
   final Map<String, V?> _cache = {};
 
-  /// _cachedKeys is only used to make sure that if you fetch all keys from a
-  /// box, you do not need to have an expensive read operation twice. There is
-  /// no other usage for this at the moment. So the cache is never partial.
-  /// Once the keys are cached, they need to be updated when changed in put and
-  /// delete* so that the cache does not become outdated.
-  Set<String>? _cachedKeys;
-
-  bool get _keysCached => _cachedKeys != null;
-
   Box(this.name, this.boxCollection);
 
   Future<List<String>> getAllKeys([Transaction? txn]) async {
-    if (_keysCached) return _cachedKeys!.toList();
     txn ??= boxCollection._db.transaction(name, 'readonly');
     final store = txn.objectStore(name);
     final request = store.getAllKeys(null);
     await request.onSuccess.first;
     final keys = request.result.cast<String>();
-    _cachedKeys = keys.toSet();
     return keys;
   }
 
@@ -151,7 +140,6 @@ class Box<V> {
     if (boxCollection._txnCache != null) {
       boxCollection._txnCache!.add((txn) => put(key, val, txn));
       _cache[key] = val;
-      _cachedKeys?.add(key);
       return;
     }
 
@@ -159,7 +147,6 @@ class Box<V> {
     final store = txn.objectStore(name);
     await store.put(val as Object, key);
     _cache[key] = val;
-    _cachedKeys?.add(key);
     return;
   }
 
@@ -167,7 +154,6 @@ class Box<V> {
     if (boxCollection._txnCache != null) {
       boxCollection._txnCache!.add((txn) => delete(key, txn));
       _cache[key] = null;
-      _cachedKeys?.remove(key);
       return;
     }
 
@@ -178,7 +164,6 @@ class Box<V> {
     // Set to null instead remove() so that inside of transactions null is
     // returned.
     _cache[key] = null;
-    _cachedKeys?.remove(key);
     return;
   }
 
@@ -188,7 +173,6 @@ class Box<V> {
       for (final key in keys) {
         _cache[key] = null;
       }
-      _cachedKeys?.removeAll(keys);
       return;
     }
 
@@ -197,7 +181,6 @@ class Box<V> {
     for (final key in keys) {
       await store.delete(key);
       _cache[key] = null;
-      _cachedKeys?.remove(key);
     }
     return;
   }
@@ -206,7 +189,6 @@ class Box<V> {
     if (boxCollection._txnCache != null) {
       boxCollection._txnCache!.add((txn) => clear(txn));
       _cache.clear();
-      _cachedKeys = null;
       return;
     }
 
@@ -214,7 +196,6 @@ class Box<V> {
     final store = txn.objectStore(name);
     await store.clear();
     _cache.clear();
-    _cachedKeys = null;
     return;
   }
 
--- lib/src/database/sqflite_box.dart
+++ lib/src/database/sqflite_box.dart
@@ -77,14 +77,6 @@ class Box<V> {
   final BoxCollection boxCollection;
   final Map<String, V?> _cache = {};
 
-  /// _cachedKeys is only used to make sure that if you fetch all keys from a
-  /// box, you do not need to have an expensive read operation twice. There is
-  /// no other usage for this at the moment. So the cache is never partial.
-  /// Once the keys are cached, they need to be updated when changed in put and
-  /// delete* so that the cache does not become outdated.
-  Set<String>? _cachedKeys;
-  bool get _keysCached => _cachedKeys != null;
-
   static const Set<Type> allowedValueTypes = {
     List<dynamic>,
     Map<dynamic, dynamic>,
@@ -141,14 +133,11 @@ class Box<V> {
   }
 
   Future<List<String>> getAllKeys([Transaction? txn]) async {
-    if (_keysCached) return _cachedKeys!.toList();
-
     final executor = txn ?? boxCollection._db;
 
     final result = await executor.query(name, columns: ['k']);
     final keys = result.map((row) => row['k'] as String).toList();
 
-    _cachedKeys = keys.toSet();
     return keys;
   }
 
@@ -244,7 +233,6 @@ class Box<V> {
     }
 
     _cache[key] = val;
-    _cachedKeys?.add(key);
     return;
   }
 
@@ -260,7 +248,6 @@ class Box<V> {
     // Set to null instead remove() so that inside of transactions null is
     // returned.
     _cache[key] = null;
-    _cachedKeys?.remove(key);
     return;
   }
 
@@ -284,7 +271,6 @@ class Box<V> {
 
     for (final key in keys) {
       _cache[key] = null;
-      _cachedKeys?.removeAll(keys);
     }
     return;
   }
@@ -299,7 +285,6 @@ class Box<V> {
     }
 
     _cache.clear();
-    _cachedKeys = null;
     return;
   }
 }
