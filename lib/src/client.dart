e9ff20aa9ed789c5b844a51c819bbdf3fc72d953
Wilson <leanhminh2907@gmail.com>
2024-07-23 11:58:27 -0400

    relax cache invalidation rule (#4)

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -2197,22 +2197,6 @@ class Client extends MatrixApi {
             EventUpdateType.accountData,
           );
         }
-
-        // invalidate space hierarchy cache
-        if (room.spaceParents.isNotEmpty) {
-          for (final space in room.spaceParents) {
-            final String? spaceId = space.roomId;
-            if (spaceId == null) continue;
-            if (spaceId == room.id) continue;
-            final Room? spaceAsRoom = getRoomById(spaceId);
-            if (spaceAsRoom == null) continue;
-            if (spaceAsRoom.spaceChildren
-                .map((child) => child.roomId)
-                .contains(room.id)) {
-              await database?.removeSpaceHierarchy(spaceId);
-            }
-          }
-        }
       }
 
       if (syncRoomUpdate is LeftRoomUpdate) {
@@ -2325,6 +2309,7 @@ class Client extends MatrixApi {
           room.setState(user);
         }
       }
+
       _updateRoomsByEventUpdate(room, update);
       if (type != EventUpdateType.ephemeral && store) {
         await database?.storeEventUpdate(update, this);
@@ -2333,6 +2318,10 @@ class Client extends MatrixApi {
         await encryption?.handleEventUpdate(update);
       }
       onEvent.add(update);
+
+      if (event.type == EventTypes.SpaceChild) {
+        await database?.removeSpaceHierarchy(room.id);
+      }
 
       if (prevBatch != null &&
           (type == EventUpdateType.timeline ||
