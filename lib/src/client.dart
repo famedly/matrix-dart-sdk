f5bc82277efb4bbb9939917d0cc00d9fbbc18945
The one with the braid <info@braid.business>
2025-02-08 08:09:12 +0100

    fix: still fetch auth metadata when well known is not served
    Signed-off-by: The one with the braid <info@braid.business>

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -620,12 +620,11 @@ class Client extends MatrixApi {
   /// The result of this call is stored in [wellKnown] for later use at runtime.
   @override
   Future<DiscoveryInformation> getWellknown() async {
-    DiscoveryInformation wellKnown;
     try {
       final wellKnownResponse = await httpClient.get(
         Uri.https(userID!.domain!, '/.well-known/matrix/client'),
       );
-      wellKnown = DiscoveryInformation.fromJson(
+      final wellKnown = DiscoveryInformation.fromJson(
         jsonDecode(utf8.decode(wellKnownResponse.bodyBytes))
             as Map<String, Object?>,
       );
@@ -634,6 +633,8 @@ class Client extends MatrixApi {
       super.homeserver = wellKnown.mHomeserver.baseUrl.stripTrailingSlash();
       _wellKnown = wellKnown;
       await database?.storeWellKnown(wellKnown);
+
+      return wellKnown;
     } finally {
       try {
         _oidcAuthMetadata = await getOidcAuthMetadata();
@@ -643,7 +644,6 @@ class Client extends MatrixApi {
         Logs().v('[OIDC] Homeserver does not support OIDC delegation.', e);
       }
     }
-    return wellKnown;
   }
 
   /// Checks to see if a username is available, and valid, for the server.
