ff7f44b64a5b0ff5a11d4f0a7fb625639f2f51c3
The one with the braid <info@braid.business>
2025-02-08 14:19:15 +0100

    fix: revert unwanted change in Client
    Signed-off-by: The one with the braid <info@braid.business>

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -2096,6 +2096,7 @@ class Client extends MatrixApi {
       // account creds
       if (account != null &&
           account['homeserver_url'] != null &&
+          account['user_id'] != null &&
           account['token'] != null) {
         _id = account['client_id'];
         homeserver = Uri.parse(account['homeserver_url']);
