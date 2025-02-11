710fbe1259f4cc63efe6ebb8f1ab9788501afade
The one with the braid <info@braid.business>
2025-02-11 14:10:33 +0100

    feat: separate OIDC discovery from .well-known
    Signed-off-by: The one with the braid <info@braid.business>

--- lib/msc_extensions/msc_3861_native_oidc/msc2965_oidc_auth_metadata/msc2965_oidc_auth_metadata.dart
+++ lib/msc_extensions/msc_3861_native_oidc/msc2965_oidc_auth_metadata/msc2965_oidc_auth_metadata.dart
@@ -5,6 +5,55 @@ import 'package:http/http.dart' hide Client;
 import 'package:matrix/matrix.dart';
 
 extension OidcProviderMetadataExtension on Client {
+  /// High-level function to get the OIDC auth metadata for the homeserver
+  ///
+  /// Performs checks on all three revisions of MSC2965 for OIDC discovery.
+  ///
+  /// In case the homeserver supports OIDC, this will store the OIDC Auth
+  /// Metadata provided by the homeserver.
+  ///
+  /// This function might usually be called by [checkHomeserver]. Works similar
+  /// to [getWellknown].
+  Future<Map<String, Object?>?> getOidcDiscoveryInformation() async {
+    Map<String, Object?>? oidcMetadata;
+
+    // MSC2965 no longer expects any information on whether OIDC is supported
+    // to be present in .well-known - the only way to figure out is sadly
+    // calling the /auth_metadata endpoint.
+
+    try {
+      oidcMetadata = await getOidcAuthMetadata();
+    } catch (e) {
+      Logs().v(
+        '[OIDC] auth_metadata endpoint not supported. '
+        'Fallback on legacy .well-known discovery.',
+        e,
+      );
+    }
+    if (oidcMetadata == null) {
+      try {
+        // even though no longer required, a homeserver *might* still prefer
+        // the fallback on .well-known discovery as per
+        // https://openid.net/specs/openid-connect-discovery-1_0.html
+        final issuer =
+            // ignore: deprecated_member_use_from_same_package
+            wellKnown?.authentication?.issuer ?? await oidcAuthIssuer();
+        // ignore: deprecated_member_use_from_same_package
+        oidcMetadata = await getOidcAuthWellKnown(issuer);
+      } catch (e) {
+        Logs().v('[OIDC] Homeserver does not support OIDC delegation.', e);
+      }
+    }
+    if (oidcMetadata == null) {
+      return null;
+    }
+
+    Logs().v('[OIDC] Found auth metadata document.');
+
+    await database?.storeOidcAuthMetadata(oidcMetadata);
+    return oidcMetadata;
+  }
+
   /// Loads the Auth Metadata from the homeserver
   ///
   /// Even though homeservers might still use the previous proposed approaches
