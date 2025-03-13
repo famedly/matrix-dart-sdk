68c69cc4da290b4eeae078129e6bd6fce927ae4e
Karthikeyan S <karthikeyan.s@famedly.com>
2025-03-13 21:30:53 +0530

    feat: export timeline events

--- lib/matrix.dart
+++ lib/matrix.dart
@@ -80,6 +80,7 @@ export 'msc_extensions/msc_3935_cute_events/msc_3935_cute_events.dart';
 export 'msc_extensions/msc_1236_widgets/msc_1236_widgets.dart';
 export 'msc_extensions/msc_2835_uia_login/msc_2835_uia_login.dart';
 export 'msc_extensions/msc_3814_dehydrated_devices/msc_3814_dehydrated_devices.dart';
+export 'msc_extensions/extension_timeline_export/timeline_export.dart';
 
 export 'src/utils/web_worker/web_worker_stub.dart'
     if (dart.library.html) 'src/utils/web_worker/web_worker.dart';
