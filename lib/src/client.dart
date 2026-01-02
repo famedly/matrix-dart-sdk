44c54ee3b199f82507f02154fbeae561ca6c90ec
Christian Kußowski <c.kussowski@famedly.com>
2026-01-02 15:31:22 +0100

    feat: (BREAKING) Store left rooms if syncfilter includes leave

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -2699,12 +2699,15 @@ class Client extends MatrixApi {
             room,
             timelineEvents,
             timelineUpdateType,
-            store: false,
+            store: syncFilter.room?.includeLeave == true,
           );
         }
         final accountData = syncRoomUpdate.accountData;
         if (accountData != null && accountData.isNotEmpty) {
           for (final event in accountData) {
+            if (syncFilter.room?.includeLeave == true) {
+              await database.storeRoomAccountData(room.id, event);
+            }
             room.roomAccountData[event.type] = event;
           }
         }
@@ -2714,7 +2717,7 @@ class Client extends MatrixApi {
             room,
             state,
             EventUpdateType.state,
-            store: false,
+            store: syncFilter.room?.includeLeave == true,
           );
         }
       }
@@ -2968,8 +2971,12 @@ class Client extends MatrixApi {
       rooms.insert(position, room);
     }
     // If the membership is "leave" then remove the item and stop here
-    else if (found && membership == Membership.leave) {
-      rooms.removeAt(roomIndex);
+    else if (membership == Membership.leave) {
+      if (syncFilter.room?.includeLeave == true && !found) {
+        rooms.add(room);
+      } else if (found) {
+        rooms.removeAt(roomIndex);
+      }
     }
     // Update notification, highlight count and/or additional information
     else if (found &&
