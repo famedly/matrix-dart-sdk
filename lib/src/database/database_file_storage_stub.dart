5776dd91ddeff28567de440f6c6f33dfd32bda1c
Christian Kußowski <c.kussowski@famedly.com>
2026-03-09 14:56:48 +0100

    feat: Store sending files in file cache for 30 days
    This makes it possible to retry
    sending a file event even
    after the app got
    restarted.

--- lib/src/database/database_file_storage_stub.dart
+++ lib/src/database/database_file_storage_stub.dart
@@ -6,17 +6,22 @@ mixin DatabaseFileStorage {
   late final Uri? fileStorageLocation;
   late final Duration? deleteFilesAfterDuration;
 
+  final Map<Uri, Uint8List> _cache = {};
+
   Future<void> storeFile(Uri mxcUri, Uint8List bytes, int time) async {
-    return;
+    if (mxcUri.scheme != 'cache') return;
+    _cache[mxcUri] = bytes;
   }
 
   Future<Uint8List?> getFile(Uri mxcUri) async {
-    return null;
+    return _cache[mxcUri];
   }
 
   Future<void> deleteOldFiles(int savedAt) async {
-    return;
+    return; // Not supported. Cache is cleared on every app restart anyway.
   }
 
-  Future<bool> deleteFile(Uri mxcUri) async => false;
+  Future<bool> deleteFile(Uri mxcUri) async {
+    return _cache.remove(mxcUri) != null;
+  }
 }
